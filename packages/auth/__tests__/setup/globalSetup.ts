db13ddb981dcf8f87275fdf401de3dfa00a501c0
Andrei Firsov <andrei.firsov@8base.com>
2020-02-17 20:20:57 +0300

    update env files.

--- packages/auth/__tests__/setup/globalSetup.ts
+++ packages/auth/__tests__/setup/globalSetup.ts
@@ -1,4 +1,8 @@
-require('dotenv').config();
+const path = require('path');
+
+require('dotenv').config({
+  path: path.resolve('../../', '.env'),
+});
 
 const { setup: setupDevServer } = require('jest-dev-server');
 const { setup: setupPuppeteer } = require('jest-environment-puppeteer');
